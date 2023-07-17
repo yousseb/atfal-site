8919880cfd35452b3488ef3b65e3d21019b624db
Youssef A. Abukwaik <youssef.adnan@gmail.com>
2023-07-16 18:04:43 -0700

    Improve docker-compose-services.yml

--- .docker/pre.sh
+++ .docker/pre.sh
@@ -3,4 +3,9 @@
 apt update && apt dist-upgrade
 apt install vim software-properties-common docker.io -y
 
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.20.0/docker-compose-linux-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 docker build . -f .docker/Dockerfile-web -tatfal-web

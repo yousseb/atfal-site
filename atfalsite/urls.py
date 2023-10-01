f4aee5a7b7d4383a80d199030178af1fb2659f31
Youssef A. Abukwaik <youssef.adnan@gmail.com>
2023-09-30 17:44:50 -0700

    Remove user agents

--- atfalsite/urls.py
+++ atfalsite/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import user_agents
+#import user_agents
 from django.contrib import admin
 from django.conf.urls.i18n import i18n_patterns
 from django.shortcuts import redirect

c8edf4e6e9b31a072d4f9d73c5ab9a3a4561088a
Youssef A. Abukwaik <youssef.adnan@gmail.com>
2023-10-12 14:52:10 -0700

    Ready for dataset

--- reunite/common/common_utils.py
+++ reunite/common/common_utils.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import requests
 
 
-def download_file(self, url: str, path: Path) -> None:
+def download_file(url: str, path: Path) -> None:
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.35 (KHTML, like Gecko) '
                       'Chrome/39.0.2171.95 Safari/537.36'}
